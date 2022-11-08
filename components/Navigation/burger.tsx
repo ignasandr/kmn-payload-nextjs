9097bad9b2a3d10c0dbdff4addec06749c5219ab
ignasandr <ignas.an@gmail.com>
2022-11-08 08:44:17 +0200

    burger div

--- components/Navigation/burger.tsx
+++ components/Navigation/burger.tsx
@@ -3,9 +3,9 @@ import styles from './styles.module.css'
 export default function Burger() {
     return (
         <div className={styles.burger}>
-            <div className={styles.burgerLine} />
-            <div className={styles.burgerLine} />
-            <div className={styles.burgerLine} />
+            <div/>
+            <div/>
+            <div/>
         </div>
     )
 }
