a174d53e510c4b0b3e1d38ee87d611635b46e962
ignasandr <ignas.an@gmail.com>
2022-11-14 22:04:19 +0200

    next 13

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { use } from "react";
 import Header from '../components/Nav/header';
 import './global.css';
 
-const inter = Inter({subsets: ["latin-ext"]});
+const inter = Inter({weight: ["100", "200", "300", "400", "500", "600", "700", "800"], subsets: ["latin-ext"]});
 
 async function getMenuItems() {
     let menu = await fetch("http://localhost:3000/api/globals/menu");
@@ -20,7 +20,9 @@ export default function RootLayout({
 
   return (
     <html>
-      <head></head>
+      <head>
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800&display=swap" rel="stylesheet"></link>
+      </head>
       <body>
         <Header menuItems={menu.menuItems}/>
         {children}
--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  // experimental:{appDir: true},
+  experimental:{appDir: true},
   reactStrictMode: true,
   swcMinify: true,
   images: {
